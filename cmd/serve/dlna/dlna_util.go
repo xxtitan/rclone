9cb329809d303e8b9c8d743bdf323bd3298e2813
Nick Craig-Wood <nick@craig-wood.com>
2026-04-18 10:35:51 +0100

    serve dlna: add Samsung-specific XML namespace
    Add xmlns:sec="http://www.sec.co.kr/" namespace to DIDL-Lite responses
    as required by Samsung TV DLNA implementations. This namespace is used
    by working DLNA servers like MediaBrowser/Emby for Samsung compatibility.
    
    Based on research of open source DLNA servers that successfully work
    with Samsung TVs.
    
    See #9346

--- cmd/serve/dlna/dlna_util.go
+++ cmd/serve/dlna/dlna_util.go
@@ -64,7 +64,8 @@ func didlLite(chardata string) string {
 		` xmlns:dc="http://purl.org/dc/elements/1.1/"` +
 		` xmlns:upnp="urn:schemas-upnp-org:metadata-1-0/upnp/"` +
 		` xmlns="urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/"` +
-		` xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/">` +
+		` xmlns:dlna="urn:schemas-dlna-org:metadata-1-0/"` +
+		` xmlns:sec="http://www.sec.co.kr/">` +
 		chardata +
 		`</DIDL-Lite>`
 }
